521c6494294b46bc881c13b718cd655064cc986b
Nickolay <nickolay@nicks-MBP.lan>
2024-06-26 07:55:51 -0400

    feat: add logo description

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,6 +4,9 @@ const Logo: React.FC = () => {
     return (
         <div className="flex items-center">
             <img src="/zello-logo.svg" alt="Zello Logo" className="h-24 w-24 mr-2" />
+            <label className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300" htmlFor="searchTerm">
+                Admin
+            </label>
         </div>
     );
 }
